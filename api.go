1b7731fd2ad04d0ee44fca81e7c61f7faa8a8b8d
matteoBuccirossi <matteobuccirossi@gmail.com>
2021-01-05 11:27:09 +0100

    🧬 fibonacci sequence enpoint

--- api.go
+++ api.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"strconv"
+	"log"
 )
 
 func main() {
@@ -12,7 +14,7 @@ func main() {
 	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		fmt.Println("Connection")
+		fmt.Println("Connected")
 		return c.SendString("hello world fiber")
 	})
 	app.Get("/api/:type", func(c *fiber.Ctx) error {
@@ -22,5 +24,29 @@ func main() {
 		return c.JSON(obj)
 	})
 
+	app.Get("/api/fib/:n/:steps", func(c *fiber.Ctx) error{
+		n, err:= strconv.Atoi(c.Params("n"))
+		steps, err:= strconv.Atoi(c.Params("steps"))
+		if err == nil{
+		  fib(n, steps)
+		  return c.SendString("fibonacci sequence")
+		}else{
+			
+			log.Fatal(err)
+			return c.SendString("invalid input")
+		}
+	})
+
 	app.Listen(":3000")
+}
+
+func fib(n int, steps int){
+	mem:= n
+	mem2:= n
+	for j:= 0; j <= steps; j++{
+		mem = mem + mem2 
+		mem2 = mem - mem2
+		fmt.Println(mem)
+	}
+
 }
